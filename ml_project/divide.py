36aae75a7fe04756ec5363b880712d5c99953e06
carlobarbini <carlo.barbini90@gmail.com>
2024-08-22 18:15:54 +0200

    deleted stuff

--- ml_project/divide.py
+++ ml_project/divide.py
@@ -2,18 +2,9 @@ def division(a, b):
     a = float(a)
     b=float(b)
     print(a / b)
-    print("this is a division")
 
-
-def mult(a, b):
-    print(a*b)
-    print("this is a mult")
-
-def this_func(a, b):
-    print(a+ b / a)
 
 if __name__=='__main__':
     division(3., 1.)
 
 
-# this is a comment
